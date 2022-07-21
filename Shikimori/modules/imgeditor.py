1d5660efb1e035a7db8056ed5f515458dd627d5e
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-07-21 16:15:22 +0530

    Update imgeditor.py

--- Shikimori/modules/imgeditor.py
+++ Shikimori/modules/imgeditor.py
@@ -113,7 +113,7 @@ async def cb_handler(client: pbot, query: CallbackQuery):
 async def photo(client: pbot, message: Message):
    await client.send_message(
       chat_id=message.chat.id,
-      text="Select your required mode from below!ㅤㅤ",
+      text="Select your required mode from below!ㅤㅤ")
       reply_to_message_id=message.reply_to_message_id
 
 
