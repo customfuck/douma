9755a859248adedf1c6277e928a8ff718b7f5ede
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-07-22 13:48:23 +0530

    Update zzparticipants.py

--- Shikimori/modules/zzparticipants.py
+++ Shikimori/modules/zzparticipants.py
@@ -5,8 +5,8 @@ from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from zerotwobot.events import register
-from zerotwobot import telethn as tbot
+from Shikimori.events import register
+from Shikimori import telethn as tbot
 
 
 @register(pattern=("/participants"))
