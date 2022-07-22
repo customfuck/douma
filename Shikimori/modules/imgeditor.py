9e0c579d5f8e349aabc0a586b522fe072f6c95dd
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-07-22 20:25:23 +0530

    Update imgeditor.py

--- Shikimori/modules/imgeditor.py
+++ Shikimori/modules/imgeditor.py
@@ -5,61 +5,16 @@ from pyrogram.types import (
     InlineKeyboardMarkup,
     Message,
 )
-from Shikimori.Extras.ImageEditor.edit_1 import (  # pylint:disable=import-error
-    black_white,
-    box_blur,
-    bright,
-    g_blur,
-    mix,
-    normal_blur,
-)
-from Shikimori.Extras.ImageEditor.edit_2 import (  # pylint:disable=import-error
-    cartoon,
-    circle_with_bg,
-    circle_without_bg,
-    contrast,
-    edge_curved,
-    pencil,
-    sepia_mode,
-    sticker,
-)
-from Shikimori.Extras.ImageEditor.edit_3 import (  # pylint:disable=import-error
-    black_border,
-    blue_border,
-    green_border,
-    red_border,
-)
-from Shikimori.Extras.ImageEditor.edit_4 import (  # pylint:disable=import-error
-    inverted,
-    removebg_plain,
-    removebg_sticker,
-    removebg_white,
-    rotate_90,
-    rotate_180,
-    rotate_270,
-    round_sticker,
-)
-from Shikimori.Extras.ImageEditor.edit_5 import (  # pylint:disable=import-error
-    normalglitch_1,
-    normalglitch_2,
-    normalglitch_3,
-    normalglitch_4,
-    normalglitch_5,
-    scanlineglitch_1,
-    scanlineglitch_2,
-    scanlineglitch_3,
-    scanlineglitch_4,
-    scanlineglitch_5,
-)
 from Shikimori import pbot
+
 
 @pbot.on_message(filters.command(["generation", "generations"]))
 async def photo(client: pbot, message: Message):
-      await client.send_message(
-            chat_id=message.chat.id,
-            text="SELECT FROM THE GENERATION U WANT TO SEE",
-            reply_markup=InlineKeyboardMarkup(
-               [
+   await client.send_message(
+      chat_id=message.chat.id,
+      text="SELECT FROM THE GENERATION U WANT TO SEE",
+      reply_markup=InlineKeyboardMarkup(
+           [
                     [
                         InlineKeyboardButton(text="GEN1", callback_data="gen1"),
                         InlineKeyboardButton(text="GEN2", callback_data="gen2"),
@@ -279,3 +234,11 @@ async def cb_handler(client: pbot, query: CallbackQuery):
                 ),
             )
             
+
+__mod_name__ = "Generation"
+
+__help__ = """
+❍ `/edit` : To edit the image.
+❍ `/editor` : To edit the imiage.
+Note: Remove BG doesn't work, dev will fix it later.
+"""
