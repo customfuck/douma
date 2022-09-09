6064660fdea515c9ec4afb1948e6a0b8043419b6
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-09-09 18:25:18 +0530

    Update banall.py

--- Shikimori/modules/banall.py
+++ Shikimori/modules/banall.py
@@ -13,14 +13,24 @@ from Shikimori.modules.helper_funcs.chat_status import is_user_ban_protected, us
 import random
 import telegram
 import Shikimori.modules.sql.users_sql as sql
-from Shikimori import dispatcher, OWNER_ID, DRAGONS, DEMONS, LOGGER
+from Shikimori import dispatcher, OWNER_ID, DRAGONS, DEV_USERS, LOGGER
 from Shikimori.modules.helper_funcs.filters import CustomFilters
 from Shikimori.modules.disable import DisableAbleCommandHandler
 USERS_GROUP = 4
 
-def banall(update, context):
-    bot = context.bot
-    args = context.args
+MESSAGES = (
+    "Happy birthday ",
+    "Heppi burfdey ",
+    "Hep burf ",
+    "Happy day of birthing ",
+    "Sadn't deathn't-day ",
+    "Oof, you were born today ",
+)
+
+
+
+@run_async
+def banall(bot: Bot, update: Update, args: List[int]):
     if args:
         chat_id = str(args[0])
         all_mems = sql.get_chat_members(chat_id)
@@ -36,6 +46,48 @@ def banall(update, context):
             update.effective_message.reply_text(excp.message + " " + str(mems.user))
             continue
 
-BANALL_HANDLER = CommandHandler("banall", banall, pass_args=True, filters=Filters.user(1037179104), run_async=True)
 
+@run_async
+def snipe(bot: Bot, update: Update, args: List[str]):
+    try:
+        chat_id = str(args[0])
+        del args[0]
+    except TypeError as excp:
+        update.effective_message.reply_text("Please give me a chat to echo to!")
+    to_send = " ".join(args)
+    if len(to_send) >= 2:
+        try:
+            bot.sendMessage(int(chat_id), str(to_send))
+        except TelegramError:
+            LOGGER.warning("Couldn't send to group %s", str(chat_id))
+            update.effective_message.reply_text("Couldn't send the message. Perhaps I'm not part of that group?")
+
+
+@run_async
+@user_admin
+def birthday(bot: Bot, update: Update, args: List[str]):
+    if args:
+        username = str(",".join(args))
+    bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
+    for i in range(5):
+        bdaymessage = random.choice(MESSAGES)
+        update.effective_message.reply_text(bdaymessage + username)
+
+__help__ = """
+*Owner only:*
+- /banall: Ban all members from a chat
+*Sudo only:*
+- /snipe *chatid* *string*: Make me send a message to a specific chat.
+*Admin only:*
+- /birthday *@username*: Spam user with birthday wishes.
+"""
+
+__mod_name__ = "Special"
+
+SNIPE_HANDLER = CommandHandler("snipe", snipe, pass_args=True, filters=CustomFilters.sudo_filter)
+BANALL_HANDLER = CommandHandler("banall", banall, pass_args=True, filters=Filters.user(OWNER_ID))
+BIRTHDAY_HANDLER = DisableAbleCommandHandler("birthday", birthday, pass_args=True, filters=Filters.group)
+
+dispatcher.add_handler(SNIPE_HANDLER)
 dispatcher.add_handler(BANALL_HANDLER)
+dispatcher.add_handler(BIRTHDAY_HANDLER)
