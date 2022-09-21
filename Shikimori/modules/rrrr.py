c37b2ca308a60a21f4910c3fc1bc0ef1b00e7a28
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-09-21 14:50:37 +0530

    Update rrrr.py

--- Shikimori/modules/rrrr.py
+++ Shikimori/modules/rrrr.py
@@ -1,28 +1,12 @@
-from telethon.tl.functions.channels import GetParticipantsRequest
-from telethon.tl.types import ChannelParticipantsSearch
-from time import sleep
-from telethon.tl.types import MessageEntityCode
-from telethon import TelegramClient, events, Button
-import telethon.sync #lol copied from docs
 import asyncio
-from telethon.tl.types import ChatBannedRights
-from telethon.tl.functions.channels import EditBannedRequest
-import logging #lol copied from docs
-import asyncio
+
 from telethon import events
 from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin
 from telethon.tl.types import ChannelParticipantCreator
-import io
-from datetime import datetime
-import configparser
-from pprint import pprint
-import argparse
-import sqlite3
-import contextlib
-import telethon
-import telethon.sync
+
+from Shikimori import telethn as client
 
 
 
